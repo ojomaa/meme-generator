3519fbfd7f26a1a20ac78b1a30d380a525bc4f93
“ojomaa” <omarjomaa21@gmail.com>
2023-11-28 13:43:20 -0500

    add meme images

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,13 +5,35 @@ import memesData from "../memesData"
 function Form() {
     const id = Math.floor(Math.random(memesData.data.memes) * memesData.data.memes.length)
     const dataImage = memesData.data.memes[id].url
+
+    // Create state for the meme image
+    const [meme, setMeme] = React.useState({
+        topText: '',
+        bottomText: '',
+        randomImage: ''
+    });
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+    // Create function that would return a random image from the memesData.js
+    function getMemeImage() {
+        const memesArray = allMemeImages.data.memes
+        const randomMeme = Math.floor(Math.random() * memesArray.length)
+        setMeme(prevState => {
+            return {
+                ...prevState,
+                randomImage: memesArray[randomMeme].url
+            }
+        })
+    }
     return (
         <main>
             <div className='Form'>
                 <input className='form-input' type='text' placeholder='Line 1'/>
                 <input className='form-input' type='text' placeholder='Line 2'/>
-                <button className='form-button' onClick={console.log(dataImage)}> Generate Meme </button>
+                <button className='form-button' onClick={getMemeImage}> Generate Meme </button>
             </div>
+            <img className='meme-image' src={meme.randomImage} />
         </main>
     )
 }
